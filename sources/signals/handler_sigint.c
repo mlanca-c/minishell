a768209a24964cef460d14c183ef9f6b25293a4b
mlanca-c <mlanca-c@student.42lisboa.com>
2021-12-12 17:12:20 +0000

    Fixed errors on Ctrl+C

--- sources/signals/handler_sigint.c
+++ sources/signals/handler_sigint.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 14:11:29 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/12/12 14:16:45 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:11:57 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ void	handler_sigint(int signum, siginfo_t *info, void *context)
 	(void)signum;
 	(void)info;
 	(void)context;
-	printf("\n"SHELL);
+	rl_replace_line("", 0);
+	printf("\n");
+	rl_on_new_line();
+	rl_redisplay();
 }
