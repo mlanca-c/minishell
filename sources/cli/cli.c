d4ed2ebdba85b513f36e31c794fe8becf35ba1fe
MLSTT <marialsottomayor@gmail.com>
2021-12-15 17:27:25 +0000

    Small changes to cli().

--- sources/cli/cli.c
+++ sources/cli/cli.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:41:22 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/12/13 15:41:23 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/12/15 17:07:42 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,8 @@ void	cli(void)
 		if (ft_strlen(line) && ft_strcmp(line, buffer))
 			add_history(line);
 		buffer = line;
+		// function name that will be at the bottom of this function and will incaminhar
+		// todo os parsing
 	}
+	rl_clear_history();
 }
