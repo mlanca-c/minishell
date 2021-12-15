c2dbeb0f3c23c71a1181dc9f8e475bcf13346003
Jose pedro Santos <zepedroabsantos@gmail.com>
2021-12-15 17:26:19 +0000

    Almost nothing added

--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josantos <josantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/14 10:50:29 by josantos          #+#    #+#             */
-/*   Updated: 2021/12/15 16:34:52 by josantos         ###   ########.fr       */
+/*   Created: 2021/12/15 12:36:05 by josantos          #+#    #+#             */
+/*   Updated: 2021/12/15 12:36:49 by josantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	echo_builtin(void)
+void	export_builtin(void)
 {
-	char	*str = "";
-	char	*suffix = "-nnnnnnn";
-
-	if (str)
-		printf("%s", str);
-	if (suffix && !ft_strncmp(suffix, "-n", 2))
-		return ;
-	else
-		printf("\n");
+	
 }
