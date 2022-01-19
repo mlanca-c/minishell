7ac373815b2642c19521db5b96eca11dba26b03b
mlanca-c <mlanca-c@student.42lisboa.com>
2022-01-19 00:26:46 +0000

    Starting recursive add nodes of ast. Seeing where things go. For now theres an error and Im not currently parsing a Simple Command. Order of ast also seems to be off. NEEDS FIXES...

--- sources/cli/cli.c
+++ sources/cli/cli.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:41:22 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/01/18 01:40:36 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/01/19 00:21:31 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,15 @@ void	controls(char *line)
 		return ;
 	controllers = init_controllers(NULL);
 	controllers->token_list = lexical_analyser(line);
-	// t_list	*token_list = controllers->token_list;
-	// while (token_list)
-	// {
-	// 	print_token(token_list->content);
-	// 	token_list = token_list->next;
-	// }
+	t_list	*token_list = controllers->token_list;
+	printf("Tokens:\n");
+	while (token_list)
+	{
+		print_token(token_list->content);
+		token_list = token_list->next;
+	}
 	controllers->parser_tree = parser();
+	printf("Parser Tree:\n");print_parser(controllers->parser_tree);
 	ft_lst_clear(controllers->token_list, free_token);
+	// ft_ast_clear(controllers->parser_tree, free_parser);
 }
