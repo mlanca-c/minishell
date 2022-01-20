27885bb2d3ca5f49e6aba11e9639d7b9532a4539
mlanca-c <mlanca-c@student.42lisboa.com>
2022-01-20 12:22:43 +0000

    deleted comments

--- sources/parser/parse_command.c
+++ sources/parser/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:31:02 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/01/19 19:56:36 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/01/20 11:15:49 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,15 +51,3 @@ t_ast	*new_command(void)
 	ft_ast_add_left(&a, ft_ast_new(cmd));
 	return (a);
 }
-
-/* t_ast	*new_command(void)
-{
-	t_ast	*ast;
-	t_cmd	*cmd;
-
-	cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
-	cmd->name = scan_token(GET)->text;
-	ast = ft_ast_new((void *)Simple_Command);
-	ft_ast_add_left(&ast, ft_ast_new(cmd));
-	return (ast);
-} */
--- sources/parser/parse_pipe.c
+++ sources/parser/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 23:43:48 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/01/19 10:59:10 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/01/20 11:15:40 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,30 +33,6 @@ t_ast	*parse_pipe(void)
 	}
 	return (NULL);
 }
-
-/* t_ast	*parse_pipe(void)
-{
-	t_ast	*a;
-	t_ast	*b;
-
-	printf("Entering parse_pipe:\n");
-	a = parse_command(); // a is an ast Simple_Command rooted
-	printf("\tparse_command: ");print_parser(a);printf("\n");
-	if (scan_token(GET)->type == PIPE)
-	{
-		scan_token(NEXT);
-		printf("\tCurrentToken: ");print_token(scan_token(GET));
-		b = parse_pipe();
-		b = parse_command();
-		printf("a: ");print_parser(a);printf("\n");
-		printf("b: ");print_parser(b);printf("\n");
-		a = new_pipeline(a, b);
-		printf("pipe: ");print_parser(a);printf("\n");
-	}
-	else if (scan_token(GET)->type == NEW_LINE)
-		return (NULL);
-	return (a);
-} */
 
 t_ast	*new_pipeline(t_ast *a, t_ast *b)
 {
