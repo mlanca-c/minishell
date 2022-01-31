1178d857653b2b90a6b0545440724d4b932b18e9
MLSTT <marialsottomayor@gmail.com>
2022-01-31 17:03:52 +0000

    Quote Removal seems ok by now

--- sources/cli/cli.c
+++ sources/cli/cli.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:41:22 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/01/25 14:15:38 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/01/31 16:53:05 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,9 @@ char	*generate_prompt(void);
 void	cli(void)
 {
 	char	*line;
-	char	*buffer;
 	char	*prompt;
 
 	signals();
-	buffer = "";
 	if (init_controllers(NULL)->debugger)
 		print_controllers();
 	while (true)
@@ -42,9 +40,7 @@ void	cli(void)
 			printf("exit\n");
 			exit_shell();
 		}
-		if (ft_strlen(line) && ft_strcmp(line, buffer))
-			add_history(line);
-		buffer = line;
+		add_history(line);
 		controls(line);
 		free(line);
 	}
--- sources/word_expansion/word_expansion.c
+++ sources/word_expansion/word_expansion.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 11:46:36 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/01/31 16:06:35 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/01/31 16:34:05 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,14 @@ void	word_expansion_str(char **name)
 {
 	if (!name)
 		return ;
-	tilde_expansion((void **)name);
-	variable_expansion((void **)name);
+	tilde_expansion((void *)name);
+	printf("tilde: %s\n", (char *)*name);
+	variable_expansion((void *)name);
+	printf("variable: %s\n", (char *)*name);
 	filename_expansion((void *)name);
-	// quote_removal((void *)name);
+	printf("file: %s\n", (char *)*name);
+	quote_removal((void *)name);
+	printf("quote: %s\n", (char *)*name);
 }
 
 /* This function handles word expansion of a t_list type */
@@ -47,9 +51,13 @@ void	word_expansion_lst(t_list *argument)
 	while (argument)
 	{
 		tilde_expansion(&(argument->content));
+		printf("tilde: %s\n", (char *)argument->content);
 		variable_expansion(&(argument->content));
+		printf("variable: %s\n", (char *)argument->content);
 		filename_expansion(&(argument->content));
-		// quote_removal(&(argument->content));
+		printf("file: %s\n", (char *)argument->content);
+		quote_removal(&(argument->content));
+		printf("quote: %s\n", (char *)argument->content);
 		argument = argument->next;
 	}
 }
