7f7db96e5c0aaffadaf8f2e0f2ce4a29602641d4
mlanca-c <mlanca-c@student.42lisboa.com>
2022-02-11 16:35:31 +0000

    Handled file redirections so far no bugs.

--- sources/debugger/print_redirection.c
+++ sources/debugger/print_redirection.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 12:09:01 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/02/11 12:18:46 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/02/11 14:58:56 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@ void	print_command_red(t_list *red)
 	static char	*type[] = {"LESS", "GREAT", "DLESS", "DGREAT", NULL};
 	t_red		*content;
 
-	if (!red)
-	{
-		printf("(null)");
-		return ;
-	}
 	printf(" [ Redirection ]:\n");
+	if (!red)
+		return ;
 	while (red)
 	{
 		content = (t_red *)red->content;
