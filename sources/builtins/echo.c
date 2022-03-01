f60613a9fc4c022ad44ee96e62625c9bb05190a5
mlanca-c <marialsottomayor@gmail.com>
2022-03-01 21:02:28 +0000

    echo and export not done yet commented for now

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -3,69 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josantos <josantos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 10:50:29 by josantos          #+#    #+#             */
-/*   Updated: 2022/01/31 17:05:13 by josantos         ###   ########.fr       */
+/*   Updated: 2022/03/01 19:44:24 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_line(t_list *lst)
-{
-	while (lst)
-	{
-		printf("%s", (char *)lst->content);
-		lst = lst->next;
-	}
-}
+// void	print_line(t_list *lst)
+// {
+// 	while (lst)
+// 	{
+// 		printf("%s", (char *)lst->content);
+// 		lst = lst->next;
+// 	}
+// }
 
-void	do_flag(char *flag, t_list *lst)
-{
-	bool	valid;
+// void	do_flag(char *flag, t_list *lst)
+// {
+// 	bool	valid;
 
-	valid = true;
-	if (flag)
-	{
-		if (ft_strncmp(flag, "n", ft_strlen(flag)))
-			valid = false;
-		if (valid == false)
-		{
-			printf("%s ", flag);
-			print_line(lst);
-			printf("\n");
-		}
-		else
-			print_line(lst);
-	}
-	else
-	{
-		print_line(lst);
-		printf("\n");
-	}
-}
+// 	valid = true;
+// 	if (flag)
+// 	{
+// 		if (ft_strncmp(flag, "n", ft_strlen(flag)))
+// 			valid = false;
+// 		if (valid == false)
+// 		{
+// 			printf("%s ", flag);
+// 			print_line(lst);
+// 			printf("\n");
+// 		}
+// 		else
+// 			print_line(lst);
+// 	}
+// 	else
+// 	{
+// 		print_line(lst);
+// 		printf("\n");
+// 	}
+// }
 
-int	echo_builtin(t_cmd *cmd)
-{
-	char	*flag;
-	t_list	*lst;
+// int	echo_builtin(t_cmd *cmd)
+// {
+// 	char	*flag;
+// 	t_list	*lst;
 
-	lst = NULL;
-	if (cmd->suffix)
-		lst = cmd->suffix;
-	flag = NULL;
-	if (lst)
-	{
-		if (!ft_strncmp(lst->content, "-n", 2))
-		{
-			flag = ft_strdup(lst->content + 1);
-			lst = lst->next;
-		}
-		do_flag(flag, lst);
-		free(flag);
-	}
-	else
-		printf("\n");
-	return (SUCCESS);
-}
+// 	lst = NULL;
+// 	if (cmd->suffix)
+// 		lst = cmd->suffix;
+// 	flag = NULL;
+// 	if (lst)
+// 	{
+// 		if (!ft_strncmp(lst->content, "-n", 2))
+// 		{
+// 			flag = ft_strdup(lst->content + 1);
+// 			lst = lst->next;
+// 		}
+// 		do_flag(flag, lst);
+// 		free(flag);
+// 	}
+// 	else
+// 		printf("\n");
+// 	return (SUCCESS);
+// }
--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -6,86 +6,86 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 12:36:05 by josantos          #+#    #+#             */
-/*   Updated: 2022/02/10 10:01:51 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/03/01 19:44:31 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_sorted_env(t_list *env)
-{
-	while (env)
-	{
-		printf("declare -x ");
-		printf("%s\n", (char *)env->content);
-		env = env->next;
-	}
-}
+// void	print_sorted_env(t_list *env)
+// {
+// 	while (env)
+// 	{
+// 		printf("declare -x ");
+// 		printf("%s\n", (char *)env->content);
+// 		env = env->next;
+// 	}
+// }
 
-void	sort_env(t_list *lst)
-{
-	char	*temp;
+// void	sort_env(t_list *lst)
+// {
+// 	char	*temp;
 
-	while (lst && lst->next)
-	{
-		if (strncmp(lst->content, lst->next->content,
-				ft_strlen(lst->content) + ft_strlen(lst->next->content)) > 0)
-		{
-			temp = lst->content;
-			lst->content = lst->next->content;
-			lst->next->content = temp;
-		}
-		lst = lst->next;
-	}
-}
+// 	while (lst && lst->next)
+// 	{
+// 		if (strncmp(lst->content, lst->next->content,
+// 				ft_strlen(lst->content) + ft_strlen(lst->next->content)) > 0)
+// 		{
+// 			temp = lst->content;
+// 			lst->content = lst->next->content;
+// 			lst->next->content = temp;
+// 		}
+// 		lst = lst->next;
+// 	}
+// }
 
-int	sorted(t_list *lst)
-{
-	int	checker;
+// int	sorted(t_list *lst)
+// {
+// 	int	checker;
 
-	checker = 0;
-	while (lst && lst->next)
-	{
-		if (strncmp(lst->content, lst->next->content,
-				ft_strlen(lst->content) + ft_strlen(lst->next->content)) > 0)
-		{
-			checker++;
-		}
-		lst = lst->next;
-		if (checker != 0)
-			break ;
-	}
-	return (checker);
-}
+// 	checker = 0;
+// 	while (lst && lst->next)
+// 	{
+// 		if (strncmp(lst->content, lst->next->content,
+// 				ft_strlen(lst->content) + ft_strlen(lst->next->content)) > 0)
+// 		{
+// 			checker++;
+// 		}
+// 		lst = lst->next;
+// 		if (checker != 0)
+// 			break ;
+// 	}
+// 	return (checker);
+// }
 
-void	do_export_suffix(t_cmd *cmd)
-{
-	char	*var;
-	t_ctrl	*controllers;
-	char	*var_name;
+// void	do_export_suffix(t_cmd *cmd)
+// {
+// 	char	*var;
+// 	t_ctrl	*controllers;
+// 	char	*var_name;
 
-	controllers = scan_controllers(NULL);
-	var = ft_strdup(cmd->suffix->content);
-	var_name = ft_strdup("");
-	if (!ft_strncmp(var, var_name, ft_strlen(var_name)))
-		ft_lst_add_back(&controllers->envp, ft_lst_new(var));
-	print_sorted_env(controllers->envp);
-}
+// 	controllers = scan_controllers(NULL);
+// 	var = ft_strdup(cmd->suffix->content);
+// 	var_name = ft_strdup("");
+// 	if (!ft_strncmp(var, var_name, ft_strlen(var_name)))
+// 		ft_lst_add_back(&controllers->envp, ft_lst_new(var));
+// 	print_sorted_env(controllers->envp);
+// }
 
-int	export_builtin(t_cmd *cmd)
-{
-	t_ctrl	*controllers;
-	t_list	*sorted_env;
+// int	export_builtin(t_cmd *cmd)
+// {
+// 	t_ctrl	*controllers;
+// 	t_list	*sorted_env;
 
-	controllers = scan_controllers(NULL);
-	if (!cmd->suffix)
-	{
-		sorted_env = controllers->envp;
-		while (sorted(sorted_env) != 0)
-			sort_env(sorted_env);
-		print_sorted_env(sorted_env);
-	}
-	else
-		do_export_suffix(cmd);
-	return (SUCCESS);
-}
+// 	controllers = scan_controllers(NULL);
+// 	if (!cmd->suffix)
+// 	{
+// 		sorted_env = controllers->envp;
+// 		while (sorted(sorted_env) != 0)
+// 			sort_env(sorted_env);
+// 		print_sorted_env(sorted_env);
+// 	}
+// 	else
+// 		do_export_suffix(cmd);
+// 	return (SUCCESS);
+// }
