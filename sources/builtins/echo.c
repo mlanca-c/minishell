17dddc0b7e97d50354442d92d68677877705c658
mlanca-c <marialsottomayor@gmail.com>
2022-03-03 17:09:59 +0000

    echo Not done yet

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -6,66 +6,49 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 10:50:29 by josantos          #+#    #+#             */
-/*   Updated: 2022/03/01 19:44:24 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/03/03 17:08:57 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	print_line(t_list *lst)
-// {
-// 	while (lst)
-// 	{
-// 		printf("%s", (char *)lst->content);
-// 		lst = lst->next;
-// 	}
-// }
+static void	do_flag(char *flag, t_list *lst);
 
-// void	do_flag(char *flag, t_list *lst)
-// {
-// 	bool	valid;
+static void	do_flag(char *flag, t_list *lst)
+{
+	if (flag && ft_strncmp(flag, "n", ft_strlen(flag)))
+		ft_lst_print(lst->next, " ");
+	else
+	{
+		ft_lst_print(lst, " ");
+		printf("\n");
+	}
+}
 
-// 	valid = true;
-// 	if (flag)
-// 	{
-// 		if (ft_strncmp(flag, "n", ft_strlen(flag)))
-// 			valid = false;
-// 		if (valid == false)
-// 		{
-// 			printf("%s ", flag);
-// 			print_line(lst);
-// 			printf("\n");
-// 		}
-// 		else
-// 			print_line(lst);
-// 	}
-// 	else
-// 	{
-// 		print_line(lst);
-// 		printf("\n");
-// 	}
-// }
+int	echo_builtin(t_cmd *command)
+{
+	char	*flag;
+	t_list	*lst;
 
-// int	echo_builtin(t_cmd *cmd)
-// {
-// 	char	*flag;
-// 	t_list	*lst;
-
-// 	lst = NULL;
-// 	if (cmd->suffix)
-// 		lst = cmd->suffix;
-// 	flag = NULL;
-// 	if (lst)
-// 	{
-// 		if (!ft_strncmp(lst->content, "-n", 2))
-// 		{
-// 			flag = ft_strdup(lst->content + 1);
-// 			lst = lst->next;
-// 		}
-// 		do_flag(flag, lst);
-// 		free(flag);
-// 	}
-// 	else
-// 		printf("\n");
-// 	return (SUCCESS);
-// }
+	lst = NULL;
+	if (!command->suffix)
+	{
+		printf("\n");
+		return (SUCCESS);
+	}
+	lst = command->suffix;
+	flag = NULL;
+	if (lst)
+	{
+		if (ft_strncmp(lst->content, "-n", 2) == 0)
+		{
+			flag = ft_strdup(lst->content + 1);
+			lst = lst->next;
+		}
+		do_flag(flag, lst);
+		free(flag);
+	}
+	else
+		printf("\n");
+	return (SUCCESS);
+}
