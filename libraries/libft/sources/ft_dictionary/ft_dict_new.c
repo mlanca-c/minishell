1f72766ff1cfe07c5143a35fe5b9e91986d768d0
mlanca-c <marialsottomayor@gmail.com>
2022-03-03 17:09:43 +0000

    Libft changes. Created some new functions. Made other functions better for project.

--- libraries/libft/sources/ft_dictionary/ft_dict_new.c
+++ libraries/libft/sources/ft_dictionary/ft_dict_new.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_dict_new.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josantos <josantos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 11:43:10 by josantos          #+#    #+#             */
-/*   Updated: 2022/02/07 11:48:41 by josantos         ###   ########.fr       */
+/*   Updated: 2022/03/03 16:20:17 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_dict	*ft_dict_new(void *key, void *content)
 {
 	t_dict	*dict;
 
+	if (!key)
+		return (NULL);
 	dict = (t_dict *)ft_calloc(1, sizeof(t_dict));
 	if (!dict)
 		return (NULL);
