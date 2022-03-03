4158aae3123a2a340bd7548aeef6dd95b21f8900
mlanca-c <marialsottomayor@gmail.com>
2022-03-03 17:10:13 +0000

    env done in one line of code!

--- sources/builtins/env.c
+++ sources/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 17:21:55 by josantos          #+#    #+#             */
-/*   Updated: 2022/03/01 20:44:00 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/03/03 16:36:23 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	env_builtin(t_cmd *command)
 {
 	(void)command;
-	ft_dict_print(scan_controllers(NULL)->envp, "%s%s\n");
+	ft_dict_print(scan_controllers(NULL)->envp, "%s%s\n", NULL);
 	return (SUCCESS);
 }
