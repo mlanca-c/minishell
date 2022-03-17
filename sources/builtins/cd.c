b40b38d370613c1bce5e086c451eeb9f9a162887
Jose pedro Santos <zepedroabsantos@gmail.com>
2022-03-17 18:29:44 +0000

    builtin return values done

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,22 +6,25 @@
 /*   By: josantos <josantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:01:46 by josantos          #+#    #+#             */
-/*   Updated: 2022/03/17 17:58:26 by josantos         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:26:28 by josantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	cd_builtin(t_cmd *command)
-{
-	static bool	first = true;
-	
+{	
 	if (!command->suffix)
 		chdir(scan_envp("HOME", NULL));
 	else if (ft_strncmp(command->suffix->content, "-",
 			ft_strlen(command->suffix->content)) == 0)
 	{
-		if (cd_builtin_minus(first) == FAILURE)
+		if (opendir(scan_envp("OLDPWD", NULL)))
+		{
+			chdir(scan_envp("OLDPWD", NULL));
+			printf("%s\n", scan_envp("OLDPWD", NULL));
+		}
+		else
 			return (FAILURE);
 	}
 	else if (opendir(command->suffix->content))
@@ -31,25 +34,7 @@ int	cd_builtin(t_cmd *command)
 		builtin_err(command, "No such file or directory");
 		return (FAILURE);
 	}
-	first = false;
 	scan_envp("OLDPWD", ft_strdup(scan_envp("PWD", NULL)));
 	scan_envp("PWD", getcwd(NULL, 0));
 	return (SUCCESS);
-}
-
-int	cd_builtin_minus(bool first)
-{
-	if (first == true)
-		{
-			printf("crash-1.0$ cd: OLDPWD not set\n");
-			return (FAILURE);
-		}
-		if (opendir(scan_envp("OLDPWD", NULL)))
-		{
-			chdir(scan_envp("OLDPWD", NULL));
-			printf("%s\n", scan_envp("OLDPWD", NULL));
-		}
-		else
-			return (FAILURE);
-		return (SUCCESS);
 }
