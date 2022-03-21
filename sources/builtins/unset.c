a6c1f5a267919c4b30c8cd94218139eecfb0969e
mlanca-c <marialsottomayor@gmail.com>
2022-03-21 17:15:22 +0000

    This was difficult... It shouldnt have been that hard...

--- sources/builtins/unset.c
+++ sources/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:18:20 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/03/04 18:59:25 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/03/21 17:14:48 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 int	unset_builtin(t_cmd *command)
 {
-	char	*key;
+	char	*k;
 	t_list	*lst;
+	t_dict	*temp;
 	t_dict	*envp;
 
-	if (!command->suffix)
-		return (SUCCESS);
 	lst = command->suffix;
 	envp = scan_controllers(NULL)->envp;
 	while (lst)
 	{
-		key = ft_strjoin(lst->content, "=");
+		temp = envp;
+		k = ft_strjoin(lst->content, "=");
 		while (envp)
 		{
-			if (!ft_strncmp(envp->key, key, ft_strlen(key))
-				|| !ft_strncmp(envp->key, lst->content, ft_strlen(envp->key)))
+			if (!ft_strcmp(envp->key, k) || !ft_strcmp(envp->key, lst->content))
 			{
 				ft_dict_remove(&envp, free);
 				break ;
 			}
 			envp = envp->next;
 		}
-		free(key);
+		free(k);
 		lst = lst->next;
+		temp = envp;
 	}
 	return (SUCCESS);
 }
